11278433ac7388eff2653e190cd17f9b0da17fdf
Mikhail Berdnikov <north@live.ru>
2021-05-12 12:00:34 +0300

    MessageEdited

--- forwarder.py
+++ forwarder.py
@@ -303,12 +303,12 @@ async def main_edit_message(to_primary_id, client, event):
                      id_message, id_channel)
         return
 
-    # message_sent = await client.send_message(
-    #     to_primary_id, message, reply_to=msg, silent=True)
+    message_sent = await client.send_message(
+        to_primary_id, message, reply_to=msg, silent=True)
 
-    # db_stats.delete_primary_message_id(id_message, id_channel)
-    # db_stats.set_primary_message_id(
-    #     message_sent.id, id_message, id_channel)
+    db_stats.delete_primary_message_id(id_message, id_channel)
+    db_stats.set_primary_message_id(
+        message_sent.id, id_message, id_channel)
 
 
 async def main_forward_message(to_primary_id, to_secondary_id, client, event):
@@ -394,9 +394,9 @@ def forward_exec(from_chat_id, to_primary_id, to_secondary_id, client):
     async def handler(event):
         await main_forward_message(to_primary_id, to_secondary_id, client, event)
 
-    # @client.on(events.MessageEdited)
-    # async def handler_edit(event):
-    #     await main_edit_message(to_primary_id, client, event)
+    @client.on(events.MessageEdited(chats=(from_chat_id)))
+    async def handler_edit(event):
+        await main_edit_message(to_primary_id, client, event)
 
 
 def get_invite_string_from_url(url):
