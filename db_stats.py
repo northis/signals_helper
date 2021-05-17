bf1aedca9d784926c4f4dfb213881baea0316dbc
Mikhail Berdnikov <north@live.ru>
2021-05-18 00:57:36 +0300

    pinned_except_channel_id

--- db_stats.py
+++ db_stats.py
@@ -411,7 +411,10 @@ async def set_pinned(client: TelegramClient, forwards, primary_chat):
     symbol = classes.Symbol.XAUUSD
     channel_ids = list()
     for forward in forwards:
-        channel_ids.append(str(forward['from_chat_id']))
+        from_chat_id = forward['from_chat_id']
+        if from_chat_id == config.PINNED_EXCEPT_CHANNEL_ID:
+            continue
+        channel_ids.append(str(from_chat_id))
 
     channels_string = ",".join(channel_ids)
     channels_stats_query_xau = f"""select
