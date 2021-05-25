084334b065a4ef2248a70e83601e2f7979c1b80e
Mikhail Berdnikov <north@live.ru>
2021-05-26 00:38:17 +0300

    symbols

--- db_stats.py
+++ db_stats.py
@@ -448,7 +448,7 @@ group by IdChannel having IdChannel in ({channels_string}) order by avg_diff des
             amount = channels_stat[8]
             # id_channel = channels_stat[9]
 
-            channel_string = f"{count}. [{name}]({link}) \n**🎯{avg_diff}** (⬆{avg_max} ⬇{avg_min})\n∑{amount}\n❌{avg_sl}\n🕑{time_h_avg} (⬆{time_h_max})"
+            channel_string = f"{count}. [{name}]({link}) \n**🎯{avg_diff}** (▲{avg_max} ▼{avg_min})\n∑{amount}\n❌{avg_sl}\n🕑{time_h_avg} (▲{time_h_max})"
             channel_strings.append(channel_string)
             count += 1
 
