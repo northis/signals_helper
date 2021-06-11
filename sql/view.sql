3663e0560d4bbd5116434c175438ac959a61db89
Mikhail Berdnikov <north@live.ru>
2021-06-11 12:15:06 +0300

    USE_FAST_BOOK

--- sql/view.sql
+++ sql/view.sql
@@ -3,7 +3,10 @@ select Date as "open", CloseDate as "close", Symbol as symbol, CASE IsBuy
                THEN 'sell'
            ELSE 
 				'buy'
-       END "action", round(PriceActual,1) as "open price", o.StopLoss as sl, o.TakeProfit as tp, c.Name, ClosePrice as "close price"
+       END "action", round(PriceActual,1) as "open price", o.StopLoss as sl, o.TakeProfit as tp, c.Name, ClosePrice as "close price",
+	 round(CASE IsBuy WHEN 0 THEN o.PriceActual - o.ClosePrice  ELSE 				o.ClosePrice - o.PriceActual       END,2) diff,
+	 o.PriceSignal as ps,
+	round(CASE IsBuy WHEN 0 THEN o.StopLoss - o.PriceSignal  ELSE 				o.PriceSignal - o.StopLoss       END,2) sl_act
 FROM 'Order' o join Channel c on (c.Id = o.IdChannel)
 where CloseDate is not null and o.ErrorState is null
-    and --IdChannel in () 
+    and IdChannel in (1317030692) 
